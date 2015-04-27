0ca1c1d1e9375f63bb62b71d54958cc0e8788449
Tim Dyck <timdyck8@gmail.com>
2015-04-27 18:00:56 -0400

    Removes generated files

--- gen/com/dyck/tim/middle/earth/index/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.dyck.tim.middle.earth.index;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}

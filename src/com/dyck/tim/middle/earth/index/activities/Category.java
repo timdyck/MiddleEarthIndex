964c6ab9d63eaa87c61953d4b261341752329461
Tim Dyck <timdyck8@gmail.com>
2015-05-05 15:08:14 -0400

    Adds category buttons
    - Removes all useless/old classes
    - Adds categories enum, and uses this to create category buttons
    for each

---
+++ src/com/dyck/tim/middle/earth/index/activities/Category.java
@@ -0,0 +1,12 @@
+package com.dyck.tim.middle.earth.index.activities;
+
+/**
+ * Created by Tim on 05/05/2015.
+ */
+public enum Category {
+    All,
+    People,
+    Places,
+    Things,
+    Other
+}
